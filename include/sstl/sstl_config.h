30e6fa260d351cb129907c958673b0cd06c1cc93
aviaconstructor <no@spam.please>
2013-08-25 14:48:33 +0400

    Created macro SSTL_CONFIG_MULTITHREADED and used it for adding string comparison operators

--- include/sstl/sstl_config.h
+++ include/sstl/sstl_config.h
@@ -1,6 +1,8 @@
 #ifndef _SSTL__SSTL_CONFIG_INCLUDED
 #define _SSTL__SSTL_CONFIG_INCLUDED
 
+///@{
+/// Whether the C++11 standard is supported by the compiler
 #if !defined(SSTL_CXX11)
     #if !defined(__cplusplus) // case if this header was somehow included into a non-C++ file
         #define SSTL_CXX11 0
@@ -10,9 +12,22 @@
         #define SSTL_CXX11 0
     #endif
 #endif
+///@}
 
+///@{
+/// Use in the multithreaded environment, add synchronization
 #if !defined(SSTL_CONFIG_MULTITHREADED)
     #define SSTL_CONFIG_MULTITHREADED 1
 #endif
+///@}
+
+///@{
+/// Provide interoperability with compiler standard library.
+///
+/// This adds conversion routines for a few SSTL types, makes easier using both libraries at the same time.
+#if !defined(SSTL_STL_INTEROPERABILITY)
+    #define SSTL_STL_INTEROPERABILITY 0
+#endif
+///@}
 
 #endif
